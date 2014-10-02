a108649d93745124ea5819404695c121778351e1
Jairo Diaz <jairo.diaz@codescrum.com>
2014-10-02 11:44:54 +0100

    Added validation of the task id for deadline command

--- lib/deadline_command.rb
+++ lib/deadline_command.rb
@@ -1,11 +1,23 @@
 class DeadlineCommand
-
-  def initialize
+  def initialize(list_of_tasks = nil)
+    @tasks = list_of_tasks
   end
 
   def execute(command_line)
     command, id = Shellwords.split(command_line)
-    return 'Please enter a task ID' if id.nil?
-    'Success'
+    return "Please enter a task ID\n" if id.nil?
+
+    task = find_task(id)
+    return "Could not find a task with an ID of #{id}.\n" unless task
+
+    "Success\n"
+  end
+
+  def find_task(id_string)
+    id = id_string.to_i
+    task = @tasks.collect { |project_name, project_tasks|
+      project_tasks.find { |t| t.id == id }
+    }.reject(&:nil?).first
+    task
   end
 end
--- spec/deadline_command_spec.rb
+++ spec/deadline_command_spec.rb
@@ -1,18 +1,28 @@
+require 'rspec'
+require 'pry'
+
+require_relative '../lib/task'
 require_relative '../lib/deadline_command'
 
 describe DeadlineCommand do
+  let(:deadline_command) { DeadlineCommand.new(@project_hash) }
+
+  before do
+    task1 = Task.new(1, 'Eat more donuts.', false)
+    @project_hash = { secrets: [task1] }
+  end
+
+  it 'should fail when the task id is missing' do
+    expect(deadline_command.execute('deadline')). to eq "Please enter a task ID\n"
+  end
+
   it 'should receive a task id' do
-    expect(DeadlineCommand.new.execute('deadline 1')). to eq 'Success'
+    expect(deadline_command.execute('deadline 1')). to eq "Success\n"
   end
 
-  it 'shoud fail when the task id is missing' do
-    expect(DeadlineCommand.new.execute('deadline')). to eq 'Please enter a task ID'
+  it 'should fail when the task id is invalid' do
+    expect(deadline_command.execute('deadline 2')). to eq "Could not find a task with an ID of 2.\n"
   end
-
-  it 'shoud fail when the task id is invalid' do
-
-  end
-
 
   it 'should set a dateline for a task'
 end
