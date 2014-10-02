92ed8d09c1b5ef046ec55e521eab6753a887d549
Jairo Diaz <jairo.diaz@codescrum.com>
2014-10-02 11:23:54 +0100

    Added paramter to deadline command

--- lib/deadline_command.rb
+++ lib/deadline_command.rb
@@ -1,3 +1,11 @@
 class DeadlineCommand
 
+  def initialize
+  end
+
+  def execute(command_line)
+    command, id = Shellwords.split(command_line)
+    return 'Please enter a task ID' if id.nil?
+    'Success'
+  end
 end
--- spec/deadline_command_spec.rb
+++ spec/deadline_command_spec.rb
@@ -1,7 +1,18 @@
 require_relative '../lib/deadline_command'
 
 describe DeadlineCommand do
-  it 'should receive a task id'
-  it 'shoud fail when the task id is invalid'
+  it 'should receive a task id' do
+    expect(DeadlineCommand.new.execute('deadline 1')). to eq 'Success'
+  end
+
+  it 'shoud fail when the task id is missing' do
+    expect(DeadlineCommand.new.execute('deadline')). to eq 'Please enter a task ID'
+  end
+
+  it 'shoud fail when the task id is invalid' do
+
+  end
+
+
   it 'should set a dateline for a task'
 end
